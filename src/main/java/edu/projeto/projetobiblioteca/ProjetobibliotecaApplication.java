b8fa6bf4eb7b3c9b44c5bb69c47e1b25b9a4ee05
kevenknigg <kevenkniggendorf@gmail.com>
2024-04-28 19:59:07 -0300

    Initial Commit

---
+++ src/main/java/edu/projeto/projetobiblioteca/ProjetobibliotecaApplication.java
@@ -0,0 +1,13 @@
+package edu.projeto.projetobiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetobibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetobibliotecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/projeto/projetobiblioteca/ProjetobibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.projeto.projetobiblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetobibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
